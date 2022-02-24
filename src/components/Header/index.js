929d377f8795a8ea769a446c86fe744278bec1e9
pedro-drosa <pedro_drosa@hotmail.com>
2022-02-24 08:41:35 -0300

    feat(Header) defining reader styles and content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,18 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+
 import styles from './styles.module.css';
 
 const Header = () => (
   <header className={styles.header}>
-    <nav className="container">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="login">Login/Criar</NavLink>
+    <nav className={`${styles.nav} container`}>
+      <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+        <Dogs />
+      </Link>
+      <Link className={styles.login} to="login">
+        Login/Criar
+      </Link>
     </nav>
   </header>
 );
