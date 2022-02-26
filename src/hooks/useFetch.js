48d39e3b8ad63698b4fbad2188284fc48fb2ea2a
pedro-drosa <pedro_drosa@hotmail.com>
2022-02-26 08:41:24 -0300

    feat(useFetch): custom hook to handle asynchronous requests
    - returns an asynchronous function for using fetch
    
    - returns an object to handle error, loading, and request response states

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react';
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const request = useCallback(async (url, options) => {
+    let json;
+    let response;
+
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (!response.ok) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      // eslint-disable-next-line no-unsafe-finally
+      return { response, json };
+    }
+  }, []);
+
+  return { data, loading, error, request };
+};
+
+export default useFetch;
--- src/pages/Login/components/LoginCreate/index.js
+++ src/pages/Login/components/LoginCreate/index.js
@@ -4,6 +4,8 @@ import { userContext } from '../../../../context/userContext';
 import Input from '../../../../components/Input';
 import Button from '../../../../components/Button';
 import useForm from '../../../../hooks/useForm';
+import useFetch from '../../../../hooks/useFetch';
+import Error from '../../../../components/Error';
 
 import { USER_POST } from '../../../../services/api';
 
@@ -13,16 +15,18 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { logIn } = useContext(userContext);
+  const { request, loading, error } = useFetch();
 
   async function handlesubmit(event) {
     event.preventDefault();
+
     const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
       password: password.value,
     });
 
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) logIn(username.value, password.value);
   }
 
@@ -33,7 +37,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="userName" {...username} />
         <Input label="E-mail" type="text" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
